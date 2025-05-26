a71245adfaf3d7faef2256a2161fd623e370789a
Priyank Bisht <priyankbish@gmail.com>
2025-05-26 18:12:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.use('/images', express.static('upload/images'))
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `${process.env.BASE_URL}/images/${req.file.filename}`
         // this url produced can then be used to acces the uploaded image
         // the image uploaded will be in images folder in upload
     });
